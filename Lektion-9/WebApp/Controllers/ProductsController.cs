2977de65ffbcb13fe6738e598218c858b691ca63
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-01-11 15:58:44 +0100

    Lektion 9 (ASP.NET Core MVC och Partial Views)

---
+++ Lektion-9/WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+	public class ProductsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Search()
+		{
+			return View();
+		}
+
+		public IActionResult Compare()
+		{
+			return View();
+		}
+
+		public IActionResult WishList()
+		{
+			return View();
+		}
+	}
+}
---
+++ Lektion-9/WebApp/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿if (document.body.clientHeight < window.innerHeight) {
+    console.log('sidan är mindre')
+    document.getElementById('footer').style.position = 'fixed'
+} else {
+    console.log('sidan är större')
+}
+    
